f80979fde36d22c278472754f75ee2ed7b7ba290
tihon-end <tihonnet@gmail.com>
2019-11-10 22:10:51 +0300

    timer

--- script.js
+++ script.js
@@ -37,6 +37,61 @@ if(target && target.classList.contains('info-header-tab')){
 }
 
 })
+   
+// timer
 
+let deadLine = '2019-10-12'
+
+function getTimeRemaining(endtime){
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+    seconds = Math.floor((t/1000) % 60 ),
+    minutes = Math.floor((t/1000/60) % 60 ),
+    hours = Math.floor((t/(1000*60*60)))
+    // days = Math.floor((t/(1000*60*60*24)))
+
+return{
+    'total': t,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+}
+
+
+}
+
+function setClock(id , endtime){
+let timer = document.getElementById(id),
+hours = timer.querySelector('.hours'),
+minutes = timer.querySelector('.minutes'),
+seconds = timer.querySelector('.seconds'),
+timeInterval = setInterval(updateClock, 1000)
+
+function updateClock(){
+    let t = getTimeRemaining(endtime)
+
+    function zero(num){
+        if(num <=9){
+            return '0' + num
+        }else{
+            return num
+        } 
+        }
+
+hours.textContent = zero(t.hours)
+minutes.textContent =zero(t.minutes)
+seconds.textContent =zero(t.seconds)
+ 
+if(t.total <= 0){
+    clearInterval(timeInterval)
+     hours.textContent = '00'
+     minutes.textContent = '00'
+     seconds.textContent = '00'
+}
+}
+
+
+
+}
+setClock('timer' , deadLine)
 })
 
