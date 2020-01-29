98ef2068a5807b7486063bd3010db8f85f877235
tihon-end <tihonnet@gmail.com>
2020-01-29 15:58:50 +0300

    test13

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ if(target && target.classList.contains('info-header-tab')){
    
 // timer
 
-let deadLine = '2019-12-31'
+let deadLine = '2020-01-29'
 
 function getTimeRemaining(endtime){
     let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -124,6 +124,89 @@ descriptionBtn.forEach(descriptionBtn => {
         document.body.style.overflow = 'hidden'
     })
 })
+// form
+
+
+
+let message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся',
+    failure: 'Что-то пошло не так...'
+}
+
+let form = document.querySelector('.main-form'),
+    formModal = document.getElementById('form'),
+    input = form.getElementsByTagName('input'),
+    statusMessage = document.createElement('div')
+
+    statusMessage.classList.add('status')
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault()     //запрет перезагрузки страницы
+    form.appendChild(statusMessage)    //сообщение о статусе
+
+    let request = new XMLHttpRequest()    //создпние и настройка запроса
+    request.open('POST', 'server.php')
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8')  // JSON формат 
+                           // 'Content-Type', 'application/x-www-form-urlencoded' обычный формат                                               
+    let formData = new FormData(form)    // отправка формы
+
+    let obj = {}
+    formData.forEach(function(value, key) {
+        obj[key] = value
+    })
+    let json = JSON.stringify(obj)
+
+    request.send(json)
+
+    request.addEventListener('readystatechange', function() {    // отслеживание статуса и вывод сообщений о статусе
+        if (request.readyState < 4) {
+            statusMessage.innerHTML = message.loading;
+        } else if(request.readyState === 4 && request.status == 200) {
+            statusMessage.innerHTML = message.success
+        } else {
+            statusMessage.innerHTML = message.failure
+        }
+    })
+
+    for (let i = 0; i < input.length; i++) {  // очистка формы
+        input[i].value = ''
+    }
+
+})
+formModal.addEventListener('submit', function(event) {
+    event.preventDefault()
+    formModal.appendChild(statusMessage)
+
+    let request = new XMLHttpRequest()
+    request.open('POST', 'server.php')
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8')
+
+    let formData = new FormData(formModal)
+
+    let obj = {}
+    formData.forEach(function(value, key) {
+        obj[key] = value
+    })
+    let json = JSON.stringify(obj)
+
+    request.send(json)
+
+    request.addEventListener('readystatechange', function() {
+        if (request.readyState < 4) {
+            statusMessage.innerHTML = message.loading;
+        } else if(request.readyState === 4 && request.status == 200) {
+            statusMessage.innerHTML = message.success
+        } else {
+            statusMessage.innerHTML = message.failure
+        }
+    })
+
+    for (let i = 0; i < input.length; i++) {
+        input[i].value = ''
+    }
 
 })
 
+})
+  
